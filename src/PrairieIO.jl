418aba71213564db376a900bfaedcb915d8c4803
Romain Franconville <romain.franconville@gmail.com>
2016-08-31 09:49:56 -0400

    Ripping of import functions from dynamicFluo

--- src/PrairieIO.jl
+++ src/PrairieIO.jl
@@ -1,5 +1,13 @@
 module PrairieIO
-
+using LightXML
+using Images
+using FileIO
 # package code goes here
+include("importPrairie.jl")
+
+export
+   importPrairie,
+   getPrairieFrames
+
 
 end # module
